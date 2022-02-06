e27dcde02d8dda8e7dee40b2eb35b255a65da078
FrancisBFTC - Developments <61401239+FrancisBFTC@users.noreply.github.com>
2022-02-06 05:53:38 -0300

    Comunicação PCI em Produção #Fase2

--- confpci.asm
+++ confpci.asm
@@ -21,7 +21,7 @@ pdata 	dd 0
 
 os_PCIEnabled  db 0
 
-init_pci:
+Init_PCI:
 	mov 	eax, 0x80000000		; Bit 31 set for 'Enabled'
 	mov 	ebx, eax
 	mov 	dx, PCI_ADDRESS
@@ -32,38 +32,6 @@ init_pci:
 	sete 	dl				; Set byte if equal, otherwise clear
 	mov 	byte [os_PCIEnabled], dl
 ret
-
-
-; -----------------------------------------------------------------------------
-; os_pci_read_reg -- Read from a register on a PCI device
-;  IN:	BL  = Bus number
-;	CL  = Device/Slot/Function number
-;	DL  = Register number (0-15)
-; OUT:	EAX = Register information
-;	All other registers preserved
-os_pci_read_reg:
-	push edx
-	push ecx
-	push ebx
-
-	shl ebx, 16			; Move Bus number to bits 23 - 16
-	shl ecx, 8			; Move Device/Slot/Fuction number to bits 15 - 8
-	mov bx, cx
-	shl edx, 2
-	mov bl, dl
-	and ebx, 0x00ffffff		; Clear bits 31 - 24
-	or ebx, 0x80000000		; Set bit 31
-	mov eax, ebx
-	mov dx, PCI_ADDRESS
-	out dx, eax
-	mov dx, PCI_DATA
-	in eax, dx
-
-	pop ebx
-	pop ecx
-	pop edx
-ret
-; -----------------------------------------------------------------------------
 
 
 	
